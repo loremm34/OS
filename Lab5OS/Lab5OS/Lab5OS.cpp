9c3ea5fd894940b37d1a861f380bb99a0cf9e847
loremm34 <161493152+loremm34@users.noreply.github.com>
2024-04-28 22:37:01 +0300

    lab5 added

---
+++ Lab5OS/Lab5OS/Lab5OS.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <string>
+
+int main()
+{
+    int n;
+    std::cout << "Enter the number of elements in array: ";
+    std::cin >> n;
+
+    std::vector<float> arr(n);
+    std::cout << "Enter the elements of array (separated by a space): ";
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+    }
+
+    HANDLE hReadPipe, hWritePipe;
+    SECURITY_ATTRIBUTES sa;
+
+    sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+    sa.lpSecurityDescriptor = NULL;
+    sa.bInheritHandle = TRUE;
+
+    if (!CreatePipe(&hReadPipe, &hWritePipe, &sa, 0)) {
+        std::cout << "Create pipe failed\n";
+        std::cout << "Press any key to finish\n";
+        std::getc(stdin);
+        return GetLastError();
+    }
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+
+    std::wstring commandLine = L"Mult.exe " + std::to_wstring((int)hReadPipe) + L" " + std::to_wstring(n);
+    LPWSTR lpCommandLine = &commandLine[0];
+
+    if (!CreateProcess(NULL, lpCommandLine, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
+        std::cout << "Create client process failed\n";
+        std::cout << "Press any key to finish\n";
+        std::getc(stdin);
+        return GetLastError();
+    }
+
+    std::cout << "Press any key to start communication...";
+    std::getc(stdin);
+    std::cout << std::endl;
+
+    for (float elem : arr) {
+        DWORD dwBytesWritten;
+        if (!WriteFile(hWritePipe, &elem, sizeof(elem), &dwBytesWritten, NULL)) {
+            std::cout << "Write to pipe failed\n";  
+            std::cout << "Press any key to finish\n";
+            std::getc(stdin);
+            return GetLastError();
+        }
+        std::cout << "Wrote to pipe: " << elem << std::endl;
+    }
+
+    CloseHandle(hReadPipe);
+    CloseHandle(hWritePipe);
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return 0;
+}
---
+++ Lab5OS/Mult/Mult.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    HANDLE hReadPipe;
+
+    hReadPipe = (HANDLE)atoi(argv[1]);
+
+    int n = std::stoi(argv[2]);
+
+    float product = 1.0f;
+    for (int i = 0; i < n; i++) {
+        float readFloat;
+        DWORD dwBytesRead;
+        if (!ReadFile(hReadPipe, &readFloat, sizeof(readFloat), &dwBytesRead, NULL)) {
+            std::cout << "Read from pipe failed\n";
+            std::cout << "Press any key to finish\n";
+            std::getc(stdin);
+            return GetLastError();
+        }
+        std::cout << "Read float from pipe: " << readFloat << std::endl;
+        product *= readFloat;
+    }
+
+    std::cout << "Product of array elements received from pipe: " << product << std::endl;
+
+    std::cout << "Press any key to exit...";
+    std::getc(stdin);
+
+    return 0;
+}
